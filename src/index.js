4babcc5d039baa49ab1d4496a7120eea0e24ac97
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-17 19:56:11 -0300

    Configured redux and dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import multi from "redux-multi";
+import promise from "redux-promise";
+import thunk from "redux-thunk";
+
+import rootReducer from "./store/rootReducer";
+
 import App from "./App";
 
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const store = applyMiddleware(multi, promise, thunk)(createStore)(
+  rootReducer,
+  devTools
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
