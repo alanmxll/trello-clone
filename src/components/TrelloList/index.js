954b3db76c55d7f2bcc293e825cff687eb54b14f
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-17 21:03:06 -0300

    Added a padding

--- src/components/TrelloList/index.js
+++ src/components/TrelloList/index.js
@@ -2,10 +2,13 @@ import React from "react";
 
 import { WrapperTrelloList, TrelloListTitle } from "./style";
 
+import TrelloCard from "../TrelloCard";
+
 export default function TrelloList({ title }) {
   return (
     <WrapperTrelloList>
       <TrelloListTitle>{title}</TrelloListTitle>
+      <TrelloCard></TrelloCard>
     </WrapperTrelloList>
   );
 }
