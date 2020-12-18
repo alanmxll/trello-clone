b8d87b06ae105e28e14f359740e1fd7104d43ed4
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-17 21:24:22 -0300

    Changed some visual details of TrelloList

--- src/components/TrelloList/style.js
+++ src/components/TrelloList/style.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const WrapperTrelloList = styled.div`
-  background-color: #ccc;
+  background-color: #ebecf0;
   border-radius: 3px;
-  width: 300px;
+  width: 250px;
+  padding: 8px;
 `;
 
 export const TrelloListTitle = styled.h4``;
