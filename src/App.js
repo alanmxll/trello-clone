1b2d093df8beee596e35f0490030338923116d83
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-18 23:26:06 -0300

    imported useSelector to get list state, and a Wrapper to encapsulate lists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import TrelloTitle from "./components/TrelloTitle";
 import TrelloList from "./components/TrelloList";
 
+import { useSelector } from "react-redux";
+
+import { WrapperLists } from "./styles/global";
+
 export default function App() {
+  let lists = useSelector((state) => state.listsReducer);
+
   return (
-    <div className="App">
+    <>
       <TrelloTitle title={"Trello Clone"} />
-      <TrelloList title={"Column name"} />
-    </div>
+      <WrapperLists>
+        {lists.map((list) => {
+          return <TrelloList title={list.title} cards={list.cards} />;
+        })}
+      </WrapperLists>
+    </>
   );
 }
