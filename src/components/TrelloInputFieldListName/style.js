7899e2b28c02b87537d0e2e1b5c51ccdcdad0e6d
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-23 23:04:52 -0300

    Created TrelloInputFieldListName

---
+++ src/components/TrelloInputFieldListName/style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const WrapperInputField = styled.div`
+  margin-left: 10px;
+  background-color: #fff;
+`;
+
+export const TextAreaInputListName = styled.textarea`
+  resize: none;
+  width: 250px;
+  height: 40px;
+  border-radius: 4px;
+  margin: 5px;
+`;
+
+export const WrapperActionButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const AddListOnBoardButton = styled.button`
+  border: 1px solid white;
+  border-radius: 4px;
+  background-color: #5aac44;
+  color: #fff;
+  width: 80px;
+  min-height: 32px;
+  height: 32px;
+  margin: 5px;
+  margin-right: 10px;
+`;
