0c8a3db2baf6fd93b4480e680448876d0c04f3d4
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-23 23:03:40 -0300

    action and reducer of addListButton

---
+++ src/store/Lists/actions/actions.js
@@ -0,0 +1,25 @@
+import { store } from "../../store";
+
+export function clickOnAddColumnButton() {
+  const currentState = store.getState();
+  let { addColumnButtonWasClicked } = currentState.listsReducer;
+
+  addColumnButtonWasClicked = true;
+
+  return {
+    type: "ON_CLICK_ADD_COLUMN_BUTTON",
+    payload: addColumnButtonWasClicked,
+  };
+}
+
+export function clickOnCloseInputFieldIcon() {
+  const currentState = store.getState();
+  let { addColumnButtonWasClicked } = currentState.listsReducer;
+
+  addColumnButtonWasClicked = false;
+
+  return {
+    type: "ON_CLICK_ADD_COLUMN_BUTTON",
+    payload: addColumnButtonWasClicked,
+  };
+}
--- src/store/Lists/reducers/reducer.js
+++ src/store/Lists/reducers/reducer.js
@@ -1,40 +1,48 @@
-const initialState = [
-  {
-    title: "Study",
-    id: 0,
-    cards: [
-      {
-        id: 0,
-        text: "Learn ReactJS and Redux",
-      },
-      {
-        id: 1,
-        text: "Studying ReactJS building a Trello Clone",
-      },
-    ],
-  },
-  {
-    title: "Recreation",
-    id: 1,
-    cards: [
-      {
-        id: 0,
-        text: "Practice physical exercises",
-      },
-      {
-        id: 1,
-        text: "Read a book",
-      },
-      {
-        id: 2,
-        text: "Play World Of Warcraft",
-      },
-    ],
-  },
-];
+const initialState = {
+  addColumnButtonWasClicked: false,
+  lists: [
+    {
+      title: "Study",
+      id: 0,
+      cards: [
+        {
+          id: 0,
+          text: "Learn ReactJS and Redux",
+        },
+        {
+          id: 1,
+          text: "Studying ReactJS building a Trello Clone",
+        },
+      ],
+    },
+    {
+      title: "Recreation",
+      id: 1,
+      cards: [
+        {
+          id: 0,
+          text: "Practice physical exercises",
+        },
+        {
+          id: 1,
+          text: "Read a book",
+        },
+        {
+          id: 2,
+          text: "Play World Of Warcraft",
+        },
+      ],
+    },
+  ],
+};
 
 const listsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "ON_CLICK_ADD_COLUMN_BUTTON":
+      return {
+        ...state,
+        addColumnButtonWasClicked: action.payload,
+      };
     default:
       return state;
   }
