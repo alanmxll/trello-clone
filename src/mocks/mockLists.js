e289f36448cdbf055ce56fa5f069fdd8df3140ae
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-01-02 21:13:52 -0300

    mockList

---
+++ src/mocks/mockLists.js
@@ -0,0 +1,36 @@
+const mockList = [
+  {
+    title: "Study",
+    id: 0,
+    cards: [
+      {
+        id: 0,
+        text: "Learn ReactJS and Redux",
+      },
+      {
+        id: 1,
+        text: "Studying ReactJS building a Trello Clone",
+      },
+    ],
+  },
+  {
+    title: "Recreation",
+    id: 1,
+    cards: [
+      {
+        id: 0,
+        text: "Practice physical exercises",
+      },
+      {
+        id: 1,
+        text: "Read a book",
+      },
+      {
+        id: 2,
+        text: "Play World Of Warcraft",
+      },
+    ],
+  },
+];
+
+export default mockList;
--- src/store/Lists/reducers/reducer.js
+++ src/store/Lists/reducers/reducer.js
@@ -1,38 +1,7 @@
+import mockList from "../../../mocks/mockLists";
+
 const initialState = {
-  lists: [
-    {
-      title: "Study",
-      id: 0,
-      cards: [
-        {
-          id: 0,
-          text: "Learn ReactJS and Redux",
-        },
-        {
-          id: 1,
-          text: "Studying ReactJS building a Trello Clone",
-        },
-      ],
-    },
-    {
-      title: "Recreation",
-      id: 1,
-      cards: [
-        {
-          id: 0,
-          text: "Practice physical exercises",
-        },
-        {
-          id: 1,
-          text: "Read a book",
-        },
-        {
-          id: 2,
-          text: "Play World Of Warcraft",
-        },
-      ],
-    },
-  ],
+  lists: mockList,
 };
 
 const listsReducer = (state = initialState, action) => {
