6873bf48e98140c158380e3eddb017af34cb55c2
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-01-29 21:12:19 -0300

    added: main menu on toolbar without logic

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,20 @@ import React from "react";
 
 import { Container } from "./styles";
 
+import { CgMenuGridO } from "react-icons/cg";
+
 import TrelloLogo from "../../assets/images/trello-logo.png";
 
 export default function Header() {
   return (
     <Container>
-      <img src={TrelloLogo} alt="logo" height={30}/>
+      <div className="menu-buttons">
+        <div className="main-menu">
+          <CgMenuGridO size={24} color={"fff"} />
+        </div>
+      </div>
+      <img src={TrelloLogo} alt="logo" height={30} />
+      <div className="setting-buttons"></div>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
   height: 45px;
   background: rgba(0, 0, 0, 0.25);
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   margin-bottom: 20px;
 
@@ -14,5 +14,28 @@ export const Container = styled.div`
     color: #fff;
     font-weight: bold;
     font-size: 18px;
+  }
+
+  div.menu-buttons {
+    display: flex;
+    margin-left: 5px;
+
+    div.main-menu {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 5px;
+      border-radius: 5px;
+      background-color: rgba(0, 0, 0, 0.25);
+      cursor: pointer;
+
+      :hover {
+        background-color: rgba(0, 0, 0, 0.35);
+      }
+
+      :active {
+        background-color: rgba(0, 0, 0, 0.5);
+      }
+    }
   }
 `;
